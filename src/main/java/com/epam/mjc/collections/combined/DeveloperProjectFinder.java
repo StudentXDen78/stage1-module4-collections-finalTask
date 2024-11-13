3822e0544af9d2d5ff41f807a05206adb383bbd1
xden78 <venera.opt@gmail.com>
2024-11-13 21:51:02 +0300

    4

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -3,13 +3,22 @@ package com.epam.mjc.collections.combined;
 import java.util.*;
 
 public class DeveloperProjectFinder {
-    public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+    public static List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
         List<String> projectList = new ArrayList<>();
         for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
             if (e.getValue().contains(developer)) projectList.add(e.getKey());
         }
         Collections.sort(projectList);
-        Collections.reverse(projectList);
+        // Collections.reverse(projectList);
         return projectList;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Set<String>> projects = new LinkedHashMap<>();
+        projects.put("CSO", Set.of("Ivan", "Anna", "Lidia", "Antony"));
+        projects.put("VVaS", Set.of("Mary", "Ben", "Max"));
+        projects.put("LJA", Set.of("Oleg", "Ivan", "Alex"));
+
+        findDeveloperProject(projects, "Ivan");
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -3,22 +3,33 @@ package com.epam.mjc.collections.combined;
 import java.util.*;
 
 public class MapFromKeysCreator {
-    public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-        Map<Integer, Set<String>> resultMap = new LinkedHashMap<>();
+    public static Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new TreeMap<>();
         Set<String> keySet = sourceMap.keySet();
-        Set<String> keySetResult = new LinkedHashSet<>();
+
         for (String e : keySet) {
-            keySetResult.add(e);
+            Set<String> keySetResult = new LinkedHashSet<>();
+            if (resultMap.containsKey(e.length())) continue;
             for (String s : keySet) {
                 if (e.length() == s.length()) {
                     keySetResult.add(s);
                 }
             }
             resultMap.put(e.length(), keySetResult);
-            keySetResult.clear();
         }
 
         return resultMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> timetable = new LinkedHashMap<>();
+        timetable.put("one", 1);
+        timetable.put("two", 2);
+        timetable.put("three", 3);
+        timetable.put("five", 4);
+        timetable.put("ten", 10);
+
+        createMap(timetable);
     }
 
 }
